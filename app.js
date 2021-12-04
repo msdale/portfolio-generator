ca2bf5e43d9fd7617d05e01412449b8729594150
Mark Dale <msdaledad@gmail.com>
2021-12-03 21:04:19 -0500

    completed 9.2.7

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const [userName, githubName] = profileDataArgs;
 
-  console.log('================');
+fs.writeFile('index.html', generatePage(userName, githubName), err => {
+  if (err) throw err;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
-printProfileData(profileDataArgs);
